39f7fe92be96ce8a99ec73734a611e20af3ef6cf
mustie-THE-Dev <simon.kamau@student.moringaschool.com>
2022-05-15 09:38:22 +0300

    added multiple choices question

--- Quiz.js
+++ Quiz.js
@@ -1,7 +1,10 @@
 {let userName = document.querySelector("#userName");
 let topText = document.querySelector("#topText");
+let helloInput = document.querySelector("#helloInput");
+
 }
 function hello() {
     topText.innerHTML = ""
     topText.innerHTML = "Hello" + userName.value + " ,  Welcome to QuizBoard.";
+    helloInput.style.display = "none"
 }
